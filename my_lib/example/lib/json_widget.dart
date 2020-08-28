f3550a50f5219079eb1ea3e8923baa76ecd12540
Nazar Tsybulskyi <nazar.tsybulskyi@inveritasoft.com>
2020-08-28 15:11:01 +0300

    fix

---
+++ my_lib/example/lib/json_widget.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+class JsonTest extends StatefulWidget {
+  @override
+  _JsonTestState createState() => _JsonTestState();
+}
+
+class _JsonTestState extends State<JsonTest> {
+  var parsesPerSecond = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Start test on next tick
+    Future.microtask(_run);
+  }
+
+  Future<void> _run() async {
+    var lastFrame = DateTime.now();
+
+    final Stopwatch watch = Stopwatch();
+
+
+    var parses = 0;
+    watch.start();
+    while (true) {
+      parseJson();
+
+      // Allow UI to update every 1s & set counter
+      //final time = DateTime.now().subtract(Duration(seconds: 1));
+      var elapsedMilliseconds = watch.elapsedMilliseconds;
+      if (elapsedMilliseconds > 1000) {
+        setState(() {
+          parsesPerSecond = parses;
+        });
+        parses = 0;
+        watch.stop();
+        await Future.microtask(() {});
+        watch.start();
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      parsesPerSecond.toString(),
+      style: TextStyle(
+          color: Colors.black,
+          fontSize: 40.0
+      ),
+    );
+  }
+}
+
+//const jsonToParse = "";
+
+//void parseJson() {
+//  // Implement parsing here
+//}
+
+
+const jsonToParse = """{
+  "debug": "on",
+  "window": {
+    "title": "Sample Konfabulator Widget",
+    "name": "main_window",
+    "width": 500,
+    "height": 500
+  }
+}""";
+
+void parseJson() {
+  json.decode(jsonToParse);
+}
--- my_lib/example/lib/main.dart
+++ my_lib/example/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:my_lib/my_lib.dart';
-import 'sum.dart';
-import 'common.dart';
-import 'dart:math';
-import 'package:flutter/foundation.dart';
+import 'package:my_lib_example/add_widget.dart';
+import 'package:my_lib_example/json_widget.dart';
 
-Random rand = Random();
+
+
 
 void main() {
   runApp(MyApp());
@@ -20,9 +18,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  final  MyLib lib = MyLib();
-  final Sum sum = Sum();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,115 +27,10 @@ class _MyAppState extends State<MyApp> {
           title: const Text('FFI example app'),
         ),
         body: Center(
-          child: Column(
-            children: <Widget>[
-              RaisedButton(
-                child: Text('with FFI'),
-                onPressed: (){
-                  runWithFFI();
-                },
-              ),
-              RaisedButton(
-                child: Text('without FFI'),
-                onPressed: (){
-                  runWithoutFFI();
-                },
-              ),
-
-              RaisedButton(
-                child: Text('compute without FFI'),
-                onPressed: () async{
-                  await runIsolates();
-                },
-              ),
-            ]
-          )
+          child: JsonTest(),
         ),
       ),
     );
   }
-
-
-  void runWithFFI(){
-    List a = [];
-    List b = [];
-    for(int i = 0 ; i < 1000000 ; i++ ){
-      a.add(rand.nextInt(100));
-      b.add(rand.nextInt(100));
-    }
-    final Stopwatch watch = Stopwatch();
-    print('Calc tracker FFI  benchmark...');
-    watch.start();
-    for(int i = 0 ; i < 1000000 ; i++ ){
-      lib.nativeAdd(a[i],b[i]);
-    }
-    watch.stop();
-    final BenchmarkResultPrinter printer = BenchmarkResultPrinter();
-    printer.addResult(
-      description: 'Calc tracker',
-      value: watch.elapsedMicroseconds.toDouble(),
-      unit: 'µs',
-      name: 'iteration',
-    );
-    printer.printToStdout();
-  }
-
-  void runWithoutFFI(){
-    List a = [];
-    List b = [];
-    for(int i = 0 ; i < 1000000 ; i++ ){
-      a.add(rand.nextInt(100));
-      b.add(rand.nextInt(100));
-    }
-    final Stopwatch watch = Stopwatch();
-    print('Calc pure dart tracker benchmark...');
-    watch.start();
-    for(int i = 0 ; i < 1000000 ; i++ ){
-      //sum.sum(rand.nextInt(100),rand.nextInt(100));
-      sum.sum(a[i],b[i]);
-    }
-    watch.stop();
-    final BenchmarkResultPrinter printer = BenchmarkResultPrinter();
-    printer.addResult(
-      description: 'Calc tracker',
-      value: watch.elapsedMicroseconds.toDouble(),
-      unit: 'µs',
-      name: 'iteration',
-    );
-    printer.printToStdout();
-  }
-
-  void runIsolates() async{
-    List a = [];
-    List b = [];
-    for(int i = 0 ; i < 1000 ; i++ ){
-      a.add(rand.nextInt(100));
-      b.add(rand.nextInt(100));
-    }
-    final Stopwatch watch = Stopwatch();
-    print('Calc Isolates tracker benchmark...');
-    watch.start();
-
-    for(int i=0 ; i<10 ; i++){
-       await compute(Sum.calcalateSum,[a,b]);
-    }
-
-
-//    for(int i = 0 ; i < 10 ; i++ ){
-//      //lib.nativeCrypto(rand.nextInt(100),rand.nextInt(100));
-//      compute(Sum.sum2,[a[i],b[i]]);
-//    }
-    watch.stop();
-    final BenchmarkResultPrinter printer = BenchmarkResultPrinter();
-    printer.addResult(
-      description: 'Calc tracker',
-      value: watch.elapsedMicroseconds.toDouble(),
-      unit: 'µs',
-      name: 'iteration',
-    );
-    printer.printToStdout();
-  }
-
-
 
 }
